326a7f7cb8f40a34024562f5a598be17293cf76c
Litvinov Mikhail <sid-4d@inbox.ru>
2017-03-31 00:17:15 +0300

    add folder Litvinov to git

---
+++ git_pull_origin.bat
@@ -0,0 +1,5 @@
+cls
+git pull origin master
+echo Press ENTER to execute the command
+Pause >nul
+
---
+++ git_push_origin.bat
@@ -0,0 +1,16 @@
+cls
+git add .
+echo Press ENTER to execute the command
+Pause >nul
+
+git status
+echo Press ENTER to execute the command
+Pause >nul
+
+git commit 
+echo Press ENTER to execute the command
+Pause >nul
+
+git push origin master
+echo Press ENTER to execute the command
+Pause >nul
