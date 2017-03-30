36728cbf77ebaee1f338516b1e43a51ee8989365
Litvinov Mikhail <sid-4d@inbox.ru>
2017-03-31 00:42:27 +0300

    add gitignor and remove push bat scripts

--- git_pull_origin.bat
+++
@@ -1,5 +0,0 @@
-cls
-git pull origin master
-echo Press ENTER to execute the command
-Pause >nul
-
--- homework_git.bat
+++
@@ -1,6 +0,0 @@
-git config --global user.name "Litvinov Mikhail"
-git config --global user.email sid-4d@inbox.ru
-git init 
-git remote add origin https://github.com/itmo-web-dev/Homework.git 
-git branch --set-upstream-to=origin/master master 
-git pull https://github.com/itmo-web-dev/Homework.git 
