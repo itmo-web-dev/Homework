5d785fe07e9c42bc05ab18cf7b769a334065a96a
Litvinov Mikhail <sid-4d@inbox.ru>
2017-04-12 19:06:19 +0300

    change for comments and begin work with fourth task

--- Litvinov/js/homework06/task03.js
+++ Litvinov/js/homework06/task03.js
@@ -9,7 +9,7 @@ console.log("in side task01.js");
 - создание копии (клонирование) переданного массива.
 */
 ;(function(){
-    window.Math_sd = {
+    window.Math_sd_learn = {
        clone_array: function(array_in){
            var result_array = [];
            for(var key in array_in){
@@ -18,14 +18,14 @@ console.log("in side task01.js");
            return result_array;
        },
        min: function(array_in){
-             for(var i=0, min=array_in[0];  i<array_in.length; i++){
+             for(var i=1, min=array_in[0];  i<array_in.length; i++){
                 if (min > array_in[i])
                     min = array_in[i];
                 }
               return min;
             },
         max: function(array_in){
-             for(var i=0, max=array_in[0]; i<array_in.length; i++){
+             for(var i=1, max=array_in[0]; i<array_in.length; i++){
                 if (max < array_in[i])
                     max = array_in[i];
                 }
@@ -45,7 +45,7 @@ var A=[1,3];
 A.smel = 100;
 A.bonus=[10, 20, 30];
 A.bonus.tupic = 100;
-var B=Math_sd.clone_array(A);
+var B=Math_sd_learn.clone_array(A);
 console.log(A === B);
 A.push(3);
 console.log(A);
@@ -54,61 +54,61 @@ console.log(B);
 
     
 
-describe("task_03 Math_sd.min", function(){
+describe("task_03 Math_sd_learn.min", function(){
     var res_correct, msg;
     var value_in = [3,6,7,8];
     res_correct=3;
     msg = "минимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-       expect(Math_sd.min(value_in)).toEqual(res_correct);
+       expect(Math_sd_learn.min(value_in)).toEqual(res_correct);
     });
  });      
- describe("task_03 Math_sd.min", function(){
+ describe("task_03 Math_sd_learn.min", function(){
     var res_correct, msg;   
     var value_in = [-10, 6,7,8];
     res_correct= -10;
     msg = "минимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-        expect(Math_sd.min(value_in)).toEqual(res_correct);
+        expect(Math_sd_learn.min(value_in)).toEqual(res_correct);
     });
 });    
 
-describe("task_03 Math_sd.max", function(){
+describe("task_03 Math_sd_learn.max", function(){
     var res_correct, msg;
     var value_in = [3,6,7,8];
     res_correct=8;
     msg = "максимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-       expect(Math_sd.max(value_in)).toEqual(res_correct);
+       expect(Math_sd_learn.max(value_in)).toEqual(res_correct);
     });
  });    
-describe("task_03 Math_sd.max", function(){
+describe("task_03 Math_sd_learn.max", function(){
     var res_correct, msg;    
     var value_in = [-10, 6,7,8];
     res_correct= 8;
     msg = "максимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-        expect(Math_sd.max(value_in)).toEqual(res_correct);
+        expect(Math_sd_learn.max(value_in)).toEqual(res_correct);
     });
 });
 
-describe("task_03 Math_sd.average", function(){
+describe("task_03 Math_sd_learn.average", function(){
     var res_correct, msg;    
     var value_in = [-10, 6,7,8];
     res_correct= 2.75;
     msg = "максимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-        expect(Math_sd.average(value_in)).toEqual(res_correct);
+        expect(Math_sd_learn.average(value_in)).toEqual(res_correct);
     });
 });    
 
-describe("task_03 Math_sd.average", function(){
+describe("task_03 Math_sd_learn.average", function(){
     var res_correct, msg;    
     var value_in = [10, 6,7,8];
     res_correct= 7.75;
     msg = "максимальное значение из {0}: {1}".replace('{1}',res_correct).replace('{0}',value_in);
     it(msg, function(){
-        expect(Math_sd.average(value_in)).toEqual(res_correct);
+        expect(Math_sd_learn.average(value_in)).toEqual(res_correct);
     });
 });
    
