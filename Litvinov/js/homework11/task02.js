4258db3f91eac9fc8e42effa4b647cbade6529d5
Litvinov Mikhail <sid-4d@inbox.ru>
2017-04-25 10:08:57 +0300

    homework first and second tasks

--- Litvinov/js/homework11/task02.js
+++ Litvinov/js/homework11/task02.js
@@ -1,42 +1,72 @@
 'use struct';
 console.log("in side task02.js");
 /*
-2. Используя смешанное наследование:
-Создать шаблон объектов «Человек».
-Свойства «Имя», «Возраст», «Пол», «Интересы».
-Метод преобразования к строке вида: «Человек: 
-Иван. Возраст: 25 лет. Пол: м. Интересы: 
-музыка, программирование.»
-Создать шаблон объектов «Студент».
-Свойства от наследованные от шаблона объектов 
-«Человек» и добавить «Институт».
-Переопределить метод преобразования к строке вида: 
-«Студент: Иван. Возраст: 25 лет. Пол: м. Интересы: 
-музыка, программирование. Обучается в ИТМО.»
+2. Написать симулятор боя. Значения текущей 
+атаки и защиты 2-х противников задаются 
+случайными значениями из выбранного диапазона. 
+Полученный урон = Текущая Атака противника - Ваша текущая защита.
+Здоровье у юнитов задано в коде.
+Необходимо после каждого шага битвы
+(обмена ударами) выводить информацию о бойцах. 
+При окончании битвы вывести статистическую 
+информацию и имя победителя.
 */
 ;(function(){
+   function Fighter(name, armor, health){
+       this._name = name;
+       this._armor = armor;
+       this._health  = health ;
+   };
+   Fighter.prototype.attack = function(min, max){
+       return getRandomInt(min, max);
+   }
+   Fighter.prototype.protect = function(attack){
+       var value =  attack - this._armor;
+       if(value >0){ 
+          this._health -=  value; 
+       };
+   }
+   Fighter.prototype.get_info = function(){
+       cmd = "palayer: "+this._name+" "+this._armor+" "+this._health
+       return cmd;
+   }
+   Fighter.prototype.is_out = function(){
+       return this._health < 0;
+   }
 
-function Person(name, years, sex, interests){
-    this._name = name;
-    this._years = years;
-    this._sex= sex;
-    this._interests= interests;
-    };
- Person.prototype.toString = function(){
-        return `Человек: ${this._name}. Возраст: ${this._years} лет. Пол: ${this._sex}. Интересы: ${this._interests}.`
-        }
-    
-function Student(name, years, sex, interests, university){
-    Person.call(this,name, years, sex, interests);
-    this._university = university;
-    };
-Student.prototype.toString = function(){
-        return `Человек: ${this._name}. Возраст: ${this._years} лет. Пол: ${this._sex}. Интересы: ${this._interests}. Обучается в ${this._university}.`
-        }
-    
-var petr= new Person("Иван",25,"м",["музыка", "программирование"] );
-console.log(petr.toString());
-var petr_student = new Student("Иван",25,"м",["музыка", "программирование"],"ИТМО" );
-console.log(petr_student.toString());   
+   function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
 
+
+   
+    var player1 = new Fighter('player01',10, 200);
+    var player2 = new Fighter('player02', 10, 200);
+    var game_end = false;
+    var round = 0;
+    
+   function fight(){
+       console.log('round: '+ round++);
+       
+       player1.protect(player2.attack(15, 25));
+       if (player1.is_out()){
+           console.log("Win: " + player2._name);
+           game_end = true;
+       }
+       player2.protect(player1.attack(15, 25));
+       if (player2.is_out()){
+           console.log("Win: " + player1._name);
+           game_end = true;
+       }
+       
+       if(game_end){
+            clearTimeout(game_time);
+       }
+       
+       console.log(player1.get_info());
+       console.log(player2.get_info());
+   }
+   var game_time = setInterval(fight,500);
 }());
